7a367fa10e7473bb66f78feab513973552055868
All <volodya22all@gmail.com>
2019-05-22 16:14:32 +0300

    Change old validation to hash validation on /visit

--- app.rb
+++ app.rb
@@ -27,20 +27,36 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
-	if !params.value?("") 
+	hh = {:username => 'Введите имя',
+		:phone => 'Введите телефон',
+		:date_time => 'Введите дату и время'}
+
+	hh.each do |key, value|
+		
+		if params[key] == ''
+			@error = value
+		end
+		
+	end
+
+	if @error == '' 
 		f = File.open("./public/users.txt", "a")
 		f.write("Name: #{@username}, Phone: #{@phone}, Date/time: #{@date_time}, Barber: #{@barber}, Color: #{@color}.\n")
 		f.close
 		erb "Dear #{@username}, we will be waiting for you at #{@date_time}. Your barber is #{@barber}, Color: #{@color}. See you! <a href=\"http://localhost:4567\">На главную</a>"		
 	else 
-		@error = 'Fill all information'
-		erb :visit
+		return erb :visit
 	end
 end
 
 post '/contacts' do
-	f = File.open("./public/contacts.txt", "a")
-	f.write("Email: #{params[:email]}, Message: #{params[:message]}.\n")
-	f.close
-	"We will send our answer to #{params[:email]}. See you! <a href=\"http://localhost:4567\">На главную</a>"		
-end
+	if !params.value?("") 
+		f = File.open("./public/contacts.txt", "a")
+		f.write("Email: #{params[:email]}, Message: #{params[:message]}.\n")
+		f.close
+		"We will send our answer to #{params[:email]}. See you! <a href=\"http://localhost:4567\">На главную</a>"		
+	else 
+		@error = 'Заполните все поля'
+		return erb :contacts
+	end
+	end
