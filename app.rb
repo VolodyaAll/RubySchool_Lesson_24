86cd76d919fc41d159bb7e100c04249f50545b69
All <volodya22all@gmail.com>
2019-05-22 14:46:12 +0300

    Code from Lesson 23

---
+++ app.rb
@@ -0,0 +1,41 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	@error = 'Something wrong'
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+	@color = params[:color]
+	
+	f = File.open("./public/users.txt", "a")
+	f.write("Name: #{@username}, Phone: #{@phone}, Date/time: #{@date_time}, Barber: #{@barber}, Color: #{@color}.\n")
+	f.close
+	erb "Dear #{@username}, we will be waiting for you at #{@date_time}. Your barber is #{@barber}, Color: #{@color}. See you! <a href=\"http://localhost:4567\">На главную</a>"		
+end
+
+post '/contacts' do
+	f = File.open("./public/contacts.txt", "a")
+	f.write("Email: #{params[:email]}, Message: #{params[:message]}.\n")
+	f.close
+	"We will send our answer to #{params[:email]}. See you! <a href=\"http://localhost:4567\">На главную</a>"		
+end
